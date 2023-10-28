0667765e96f4ef3f6da9b543d4f4d358d839ec64
Gidi233 <1989871763@qq.com>
2023-10-28 14:54:05 +0800

    pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,24 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void mywalk(int level, pagetable_t pagetable)
+{
+  for (int i = 0; i < 512;i++){
+    // int index = PX(level, i);
+    pte_t pte = pagetable[i];
+    if(pte&PTE_V)
+    {
+      for (int l = 2; l >= level; l--)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte,PTE2PA(pte));
+      if (level)
+        mywalk(level - 1,(pagetable_t)PTE2PA(pte));
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n",pagetable);
+  mywalk(2, pagetable);
+}
