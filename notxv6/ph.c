a9e24d0adbca23d6eaf02e6ceebdddd06ea8b694
Gidi233 <1989871763@qq.com>
2023-12-04 09:19:29 +0800

    Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock[NBUCKET];//给每个桶加个锁，写入的时候先拿锁
 
 double
 now()
@@ -42,6 +42,7 @@ void put(int key, int value)
   int i = key % NBUCKET;
 
   // is the key already present?
+  pthread_mutex_lock(&lock[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
@@ -54,7 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -105,6 +106,9 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+for(int i = 0; i < NBUCKET; i++) {
+  pthread_mutex_init(&lock[i], NULL);
+}
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
